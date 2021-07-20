424323e685d2f52cea5b807fab6ad3cd2da96f61
danyeric123 <dnagarpowers@gmail.com>
2021-07-20 17:00:34 -0400

    models created

--- models/profile.js
+++ models/profile.js
@@ -1,14 +1,39 @@
 import mongoose from 'mongoose'
 
 export {
-  Profile
+  Profile,
+  Message
 }
+
+const replySchema = new mongoose.Schema({
+  text: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  }
+})
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  },
+  category: String,
+  replies: [replySchema]
+},{
+  timestamps: true
+})
 
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  messages: [messageSchema],
+  bio: String,
+  friends: [{ type: Schema.Types.ObjectId, ref: "Profile"}]
 }, {
   timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
+const Message = mongoose.model('Message', messageSchema)
